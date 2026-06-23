6430e55b5a05755f15dce8a442214b30c50c2dad
muhammed-ahmed-hassouna <psmohammad780@gmail.com>
2026-06-23 15:14:30 +0300

    feat : initial chromadb config

---
+++ db/chroma_client.py
@@ -0,0 +1,12 @@
+import chromadb
+import os
+
+CHROMA_DB_PATH = os.path.join(os.getcwd(), "chroma_db")
+
+client = chromadb.PersistentClient(path=CHROMA_DB_PATH)
+
+def get_collection(name: str = "knowledge_base"):
+    """
+    Get or create a ChromaDB collection by name.
+    """
+    return client.get_or_create_collection(name)
